2d2ed2df3f19043c4172985664c680fcf7222a8d
Cinder <send.cinder@gmail.com>
2021-12-13 20:54:45 -0800

    Added update script

---
+++ tools/updateCode.js
@@ -0,0 +1,32 @@
+const baseURL = "https://raw.githubusercontent.com/Cinder-dev/bitburner/main";
+
+const files = [
+    "/cc/hacknet/hacknetModule.js",
+    "/cc/commandCenter.js",
+    "/cc/module.js",
+    "/hacks/master.js",
+    "/hacks/worker.js",
+    "/tools/backdoor.js",
+    "/tools/distribute.js",
+    "/tools/replicate.js",
+    "/tools/serverCost.js",
+    "/tools/status.js",
+    "/tools/updateCode.js",
+];
+
+/** {NS} ns Scripting Runtime */
+export async function main(ns) {
+    ns.toast("Updating Code From Github", "info");
+    for (const file of files) {
+        if(await ns.wget(baseURL + file, file, "home")) {
+            // Success
+            ns.toast(`Updated ${file} from github.`, "success");
+        } else {
+            // Fail
+            ns.tprint(`Failed to get ${file} from github.`);
+            ns.toast(`Failed to get ${file} from github.`, "error");
+        }
+        await ns.sleep(200);
+    }
+    ns.toast("Finished Updating Code From Github", "info");
+}
