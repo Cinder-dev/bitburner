2e667a6140c4d81f6caf4d968cb3a0769ed14f06
Cinder <send.cinder@gmail.com>
2021-12-14 21:35:44 -0800

    Server status test

--- cc/servers/serversModule.js
+++ cc/servers/serversModule.js
@@ -1,6 +1,5 @@
 import { Module } from '/cc/module.js';
 import { getAllServers } from '/util/lists.js';
-import { scan } from '/tools/find.js';
 
 let isRunning = false;
 
@@ -31,14 +30,39 @@ export class ServersModule extends Module {
 	 */
 	constructor(ns, port) {
 		super(ns, "Servers", port, 100);
+		/** @type {NS} ns */
+		this.ns;
 		this.servers = [];
+		this.updatePort = 18;
 	}
 
 	async update(command, status) {
-		let hostnames = getAllServers(this.ns);
-		let servers = hostnames.map(hostname => this.ns.getServer(hostname)).filter(s => s.hasAdminRights);
+		this.getUsefullServers();
+		
+		let handle = this.ns.getPortHandle(this.updatePort);
+		while(!handle.empty()) {
+			let data = this.ns.readPort(this.updatePort);
+			let info = JSON.parse(data);
+
+			this.servers[info.hostname].action = info.action;
+		}
 
 		this.ns.clearLog();
-		this.ns.print(servers.map(s => s.hostname).join("\n"));
+		this.ns.print(this.servers.map(s => `${s.hostname}: ${s.action}`).join("\n"));
+	}
+
+	getUsefullServers() {
+		let hostnames = getAllServers(this.ns);
+		let servers = hostnames.map(hostname => this.ns.getServer(hostname))
+			.filter(s => s.hasAdminRights)
+			.filter(s => s.maxRam != 0);
+		servers.forEach(s => {
+			if (this.servers[s.hostname] == null) {
+				this.servers[s.hostname] = {
+					hostname: s.hostname,
+					action: "Unknown",
+				}
+			}
+		});
 	}
 }
--- hacks/worker.js
+++ hacks/worker.js
@@ -11,14 +11,17 @@ export async function main(ns) {
 		let msg = "";
 		let variant = "success";
 		if(serverInfo.hackDifficulty > securityThreshold) {
+			await ns.tryWritePort(18, JSON.stringify({hostname: serverInfo.hostname, action: "Weaken"}));
 			let weakened = await ns.weaken(serverInfo.hostname);
 			msg = `${host}: Weakened ${serverInfo.hostname} by ${weakened.toFixed(2)}`;
 			variant = "warning";
 		} else if(serverInfo.moneyAvailable < moneyThreshold) {
+			await ns.tryWritePort(18, JSON.stringify({hostname: serverInfo.hostname, action: "Grow"}));
 			let growth = await ns.grow(serverInfo.hostname);
 			msg = `${host}: Added ${ns.nFormat(serverInfo.moneyAvailable * (growth - 1), '($0.00a)')} to ${serverInfo.hostname}`;
 			variant = "info"
 		} else {
+			await ns.tryWritePort(18, JSON.stringify({hostname: serverInfo.hostname, action: "Hack"}));
 			let stolen = await ns.hack(serverInfo.hostname);
 			msg = `${host}: Stolen ${ns.nFormat(stolen, '($0.00a)')} from ${serverInfo.hostname}`;
 			variant = "success";
